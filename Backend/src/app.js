adc299b4b5e88ff34ff2e899e767421aad087991
Kunal Gore <gorekunal665@gmail.com>
2026-07-17 00:27:30 +0530

    User Module Completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+
 const authRoutes = require('./routes/auth.routes');
+const usersRoutes = require("./routes/users.routes");
+
 const { startCleanupJob } = require("./services/cleanup.service");
 
 const app = express();
@@ -11,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', usersRoutes);
+
 startCleanupJob();
 
 module.exports = app;
