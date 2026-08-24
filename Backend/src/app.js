eb54920580dc251eb11f8a8981a6c0ac534e28bc
Kunal Gore <gorekunal665@gmail.com>
2026-08-24 19:08:49 +0530

    Attendance module implemented

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const usersRoutes = require("./routes/users.routes");
 const coordinatorsRoutes = require("./routes/coordinators.routes");
+const attendanceRoutes = require("./routes/attendance.routes");
 
 const { startCleanupJob } = require("./services/cleanup.service");
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/coordinators', coordinatorsRoutes);
+app.use('/api/attendance', attendanceRoutes);
 
 startCleanupJob();
 
